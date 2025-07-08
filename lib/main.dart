f8ea4d101d0a1a1b1fbfe8fc0236a666cf91bcca
darkphantomgamerz <darkphantomgamerz@gmail.com>
2025-07-08 12:35:35 +0000

    SignIn Page built

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext cvcontext) {
-    return MaterialApp(
-      home: Home(),
-    );
+    return MaterialApp(home: Home());
   }
 }
 
@@ -20,9 +18,97 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("hello"),
+    final mediaQueryData = MediaQuery.of(context);
+    final screenWidth = mediaQueryData.size.width;
+    final screenHeight = mediaQueryData.size.height;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Welcome Back",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: screenHeight * 0.01),
+              const Text(
+                "Sign in to your account",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 174, 170, 170),
+                  fontSize: 14,
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.05),
+              SizedBox(
+                width: screenWidth * 0.85,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.02),
+              SizedBox(
+                width: screenWidth * 0.85,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: screenHeight * 0.02),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  fixedSize: WidgetStatePropertyAll(
+                    Size(screenWidth * 0.85, screenHeight * 0.09),
+                  ),
+                  backgroundColor: WidgetStatePropertyAll(
+                    const Color.fromARGB(255, 53, 115, 238),
+                  ),
+                ),
+                child: const Text(
+                  "Sign in",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account?",
+                    style: TextStyle(color: Color.fromARGB(255, 174, 170, 170)),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 53, 115, 238),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
