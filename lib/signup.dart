dffe6df82197f2649bb0727f539109895943bc01
darkphantomgamerz <darkphantomgamerz@gmail.com>
2025-07-08 15:38:18 +0000

    signup_page_start

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+class TxtField {
+  final String hintText;
+  final bool pass;
+  const TxtField({super.key, required this.hintText, required this.pass});
+}
+
 class Signup extends StatelessWidget {
   const Signup({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [Text("Create Account"), Text("Join our community")],
+          ),
+        ),
+      ),
+    );
   }
 }
